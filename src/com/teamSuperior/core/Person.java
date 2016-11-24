22ee3fa0799193d2d9acc8b23597b8dc8f21a876
Rajmund <rajmund.st@gmail.com>
2016-11-24 10:49:36 +0100

    added Person class

---
+++ src/com/teamSuperior/core/Person.java
@@ -0,0 +1,51 @@
+package com.teamSuperior.core;
+
+/**
+ * Created by Domestos Maximus on 24-Nov-16.
+ */
+public abstract class Person {
+    private String name, surname, street, city, zip, email, phoneNo, password;
+
+    public Person(String name, String surname, String street, String city, String zip, String email, String phoneNo, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.street = street;
+        this.city = city;
+        this.zip = zip;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
