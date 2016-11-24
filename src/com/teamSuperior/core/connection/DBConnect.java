fdce082902a70a6cb1054bd4dd6df47d521933e0
Rajmund <rajmund.st@gmail.com>
2016-11-24 12:34:38 +0100

    created database connection class

---
+++ src/com/teamSuperior/core/connection/DBConnect.java
@@ -0,0 +1,47 @@
+package com.teamSuperior.core.connection;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * Created by Domestos Maximus on 24-Nov-16.
+ */
+public class DBConnect {
+
+    /***
+     * Connects to thee database
+     * @return returns connection object
+     */
+    public static Connection connect(){
+        String hostName = "";
+        String userName = "";
+        String passWord = "";
+        Connection con = null;
+
+        try{
+            con = DriverManager.getConnection(hostName, userName, passWord);
+        }catch (SQLException ex){
+            //getErrorMessage
+            System.out.println(ex.getMessage());
+        }
+        return con;
+    }
+
+    /***
+     * Executes specified SQL query and returns the data from the table
+     * @param query
+     * @return ResultSet object
+     */
+    public static ResultSet getFromDataBase(String query){
+        Connection con = connect();
+        ResultSet rs = null;
+        try{
+            Statement statement = con.createStatement();
+            rs = statement.executeQuery(query);
+        }catch (SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return rs;
+    }
+
+}
