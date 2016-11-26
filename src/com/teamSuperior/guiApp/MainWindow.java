9707cdde76accbcfb08cae1a51c882856c639757
BlackThunderStudio <rajmund.st@gmail.com>
2016-11-26 22:01:57 +0100

    deleted a TODO

--- src/com/teamSuperior/guiApp/MainWindow.java
+++ src/com/teamSuperior/guiApp/MainWindow.java
@@ -13,7 +13,6 @@ public class MainWindow extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("layout/mainWindow.fxml"));
-        //TODO: fix the FXMLLoader as it doesn't read the layout file properly!!!
         primaryStage.setTitle("Team Superior - representing Silvan Inc.");
 
         primaryStage.setScene(new Scene(root, 800, 600));
