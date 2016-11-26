eb42999bd6ba0ef8072028a342ba36b589929159
BlackThunderStudio <rajmund.st@gmail.com>
2016-11-26 23:08:57 +0100

    added application exit protection

---
+++ src/com/teamSuperior/guiApp/GUI/ConfirmBox.java
@@ -0,0 +1,48 @@
+package com.teamSuperior.guiApp.GUI;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * Created by Domestos on 16.11.26.
+ */
+public class ConfirmBox {
+    private static boolean answer;
+    public static boolean display(String title, String message){
+        Stage window = new Stage();
+        answer = false;
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        Label label = new Label(message);
+        Button btnYes = new Button("Yes");
+        Button btnNo = new Button("No");
+        btnYes.setOnAction(e -> {
+            answer = true;
+            window.close();
+        });
+        btnNo.setOnAction(e -> {
+            answer = false;
+            window.close();
+        });
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, btnYes, btnNo);
+        layout.setAlignment(Pos.CENTER);
+        layout.setPadding(new Insets(10, 10, 10, 10));
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+
+        return answer;
+    }
+}
--- src/com/teamSuperior/guiApp/MainWindow.java
+++ src/com/teamSuperior/guiApp/MainWindow.java
@@ -1,5 +1,6 @@
 package com.teamSuperior.guiApp;
 
+import com.teamSuperior.guiApp.GUI.ConfirmBox;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,12 +11,27 @@ import javafx.stage.Stage;
  * Created by Domestos on 16.11.26.
  */
 public class MainWindow extends Application {
+    private Stage window;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("layout/mainWindow.fxml"));
         primaryStage.setTitle("Team Superior - representing Silvan Inc.");
 
+        primaryStage.setOnCloseRequest(e -> {
+            e.consume();
+            closeProgram();
+        });
+
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+    }
+
+    private void closeProgram(){
+        boolean answer = ConfirmBox.display("Closing the program", "There might be unsaved changes. Are you sure you want to close the application?");
+        if(answer){
+            window.close();
+        }
     }
 }
