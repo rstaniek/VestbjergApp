4d7414596dfdf1e83a6af229602817570d5661cd
BlackThunderStudio <rajmund.st@gmail.com>
2016-11-26 23:34:28 +0100

    tweaked a bit layout

--- src/com/teamSuperior/guiApp/GUI/ConfirmBox.java
+++ src/com/teamSuperior/guiApp/GUI/ConfirmBox.java
@@ -5,6 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -16,6 +17,10 @@ public class ConfirmBox {
     private static boolean answer;
     public static boolean display(String title, String message){
         Stage window = new Stage();
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10));
+        grid.setVgap(8);
+        grid.setHgap(10);
         answer = false;
 
         window.initModality(Modality.APPLICATION_MODAL);
@@ -23,8 +28,11 @@ public class ConfirmBox {
         window.setMinWidth(250);
 
         Label label = new Label(message);
+        GridPane.setConstraints(label, 0, 0);
         Button btnYes = new Button("Yes");
+        GridPane.setConstraints(btnYes, 0, 1);
         Button btnNo = new Button("No");
+        GridPane.setConstraints(btnNo, 1, 1);
         btnYes.setOnAction(e -> {
             answer = true;
             window.close();
@@ -34,13 +42,15 @@ public class ConfirmBox {
             window.close();
         });
 
-        VBox layout = new VBox(10);
-        layout.getChildren().addAll(label, btnYes, btnNo);
-        layout.setAlignment(Pos.CENTER);
-        layout.setPadding(new Insets(10, 10, 10, 10));
+        grid.getChildren().addAll(label, btnNo, btnYes);
+        //VBox layout = new VBox(10);
+        //layout.getChildren().addAll(label, btnYes, btnNo);
+        //layout.setAlignment(Pos.CENTER);
+        //layout.setPadding(new Insets(10, 10, 10, 10));
 
-        Scene scene = new Scene(layout);
+        Scene scene = new Scene(grid);
         window.setScene(scene);
+        window.setResizable(false);
         window.showAndWait();
 
         return answer;
