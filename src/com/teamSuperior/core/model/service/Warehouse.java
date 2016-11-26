3ea56d8242a1fb2f98d22613d6986a0647e62459
Smoothini <katona.andrei@gmail.com>
2016-11-26 19:50:50 +0100

    ~

---
+++ src/com/teamSuperior/core/model/service/Warehouse.java
@@ -0,0 +1,22 @@
+package com.teamSuperior.core.model.service;
+
+/**
+ * Created by Smoothini on 26.11.2016.
+ * get bitches
+ */
+public class Warehouse
+{
+    private int freeSpace;
+    private String products, lastCheckup;
+
+
+    public int getFreeSpace() { return freeSpace; }
+
+    public String getProducts() { return products; }
+
+    public String getLastCheckup() { return lastCheckup; }
+
+}
+/***
+ * fuck money
+ */
