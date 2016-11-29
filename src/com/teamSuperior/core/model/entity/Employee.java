883a3161dac63c1d249323d6da63c9bd60fc3b77
Jakub Slíva <sliva.jakub@gmail.com>
2016-11-29 13:00:09 +0100

    The employee fields are now matching to the database fields.

--- src/com/teamSuperior/core/model/entity/Employee.java
+++ src/com/teamSuperior/core/model/entity/Employee.java
@@ -23,12 +23,16 @@ public class Employee{
     private static int id = 0;
     private String name;
     private String surname;
-    private String street;
+    private String address;
     private String city;
     private String zip;
     private String email;
     private String phone;
     private String password;
+    private String position;
+    private int numberOfSales;
+    private double totalRevenue;
+    private int accessLevel;
     public Employee() {
         try
         {
@@ -39,12 +43,16 @@ public class Employee{
             {
                 this.name = res.getString(2);
                 this.surname = res.getString(3);
-                this.street = res.getString(4);
+                this.address = res.getString(4);
                 this.city = res.getString(5);
                 this.zip = res.getString(6);
                 this.email = res.getString(7);
                 this.phone = res.getString(8);
                 this.password = res.getString(9);
+                this.position = res.getString(10);
+                this.numberOfSales = res.getInt(11);
+                this.totalRevenue = res.getDouble(12);
+                this.accessLevel = res.getInt(13);
             }
         }
         catch(SQLException ex)
@@ -73,12 +81,12 @@ public class Employee{
         this.surname = surname;
     }
 
-    public String getStreet() {
-        return street;
+    public String getAddress() {
+        return address;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getCity() {
@@ -121,5 +129,35 @@ public class Employee{
         this.password = password;
     }
 
+    public String getPosition() {
+        return position;
+    }
 
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getNumberOfSales() {
+        return numberOfSales;
+    }
+
+    public void setNumberOfSales(int numberOfSales) {
+        this.numberOfSales = numberOfSales;
+    }
+
+    public double getTotalRevenue() {
+        return totalRevenue;
+    }
+
+    public void setTotalRevenue(double totalRevenue) {
+        this.totalRevenue = totalRevenue;
+    }
+
+    public int getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(int accessLevel) {
+        this.accessLevel = accessLevel;
+    }
 }
--- src/com/teamSuperior/core/model/service/Contractor.java
+++ src/com/teamSuperior/core/model/service/Contractor.java
@@ -6,6 +6,78 @@ import java.sql.ResultSet;
 /**
  * Created by Domestos Maximus on 24-Nov-16.
  */
-public class Contractor {
 
+public class Contractor {
+    private static int id;
+    private String name;
+    private String address;
+    private String city;
+    private String zip;
+    private String phone;
+    private String email;
+
+    public Contractor(String name, String address, String city, String zip, String phone, String email) {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        Contractor.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
