655e325ba96a51410667be96690a62672e53f09c
Smoothini <katona.andrei@gmail.com>
2016-12-01 10:39:11 +0100

    created Offer model

---
+++ src/com/teamSuperior/tuiApp/modelLayer/Offer.java
@@ -0,0 +1,58 @@
+package com.teamSuperior.tuiApp.modelLayer;
+
+/**
+ * Created by Smoothini on 01.12.2016.
+ */
+public class Offer {
+    private int id, productId;
+    private String date;
+    private double price, discount;
+
+    public Offer(int id, int productId, String date, double price, double discount) {
+        this.id = id;
+        this.productId = productId;
+        this.date = date;
+        this.price = price;
+        this.discount = discount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+}
