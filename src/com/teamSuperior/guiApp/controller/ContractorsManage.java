a6d46ddc9248142c781d09d42ca3a29e7f62a08b
Rajmund <rajmund.st@gmail.com>
2016-12-09 12:39:00 +0100

    added table to the window

--- src/com/teamSuperior/guiApp/controller/ContractorsManage.java
+++ src/com/teamSuperior/guiApp/controller/ContractorsManage.java
@@ -1,7 +1,26 @@
 package com.teamSuperior.guiApp.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Domestos Maximus on 09-Dec-16.
  */
-public class ContractorsManage {
+public class ContractorsManage implements Initializable {
+    @FXML
+    public TableView tableView_contractors;
+
+    @FXML
+    public void tableView_contractors_onMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
