0df85342a83afca0a3e4553b38523039a0ada384
Smoothini <katona.andrei@gmail.com>
2016-12-12 19:28:52 +0100

    created leaseMachineContainer

---
+++ src/com/teamSuperior/tuiApp/modelLayer/LeaseMachineContainer.java
@@ -0,0 +1,20 @@
+package com.teamSuperior.tuiApp.modelLayer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Smoothini on 12.12.2016.
+ */
+public class LeaseMachineContainer {
+    private static LeaseMachineContainer ourInstance = new LeaseMachineContainer();
+
+    public static LeaseMachineContainer getInstance() {
+        return ourInstance;
+    }
+
+    private ArrayList<LeaseMachine> leaseMachines;
+
+    private LeaseMachineContainer() { leaseMachines = new ArrayList<LeaseMachine>(); }
+
+    public ArrayList<LeaseMachine> getLeaseMachines() { return leaseMachines; }
+}
