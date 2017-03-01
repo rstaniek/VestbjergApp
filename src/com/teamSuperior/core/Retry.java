f625ecd01a8a666ce51da66700719ee06d25128b
Rajmund Staniek <rajmund.st@gmail.com>
2017-03-01 17:56:26 +0100

    some tests are retried after they fail

---
+++ src/com/teamSuperior/core/Retry.java
@@ -0,0 +1,42 @@
+package com.teamSuperior.core;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+/**
+ * Created by rajmu on 17.03.01.
+ */
+public class Retry implements TestRule {
+
+    private int rc;
+
+    public Retry(int rc) {
+        this.rc = rc;
+    }
+
+    @Override
+    public Statement apply(Statement statement, Description description) {
+        return statement(statement, description);
+    }
+
+    private Statement statement(final Statement base, final Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                Throwable throwable = null;
+                for (int i = 0; i < rc; i++) {
+                    try {
+                        base.evaluate();
+                        return;
+                    } catch (Throwable e) {
+                        throwable = e;
+                        System.err.println(String.format("Run %d failed!", i + 1));
+                    }
+                }
+                System.out.println(String.format("Giving up after %d failures.", rc));
+                throw throwable;
+            }
+        };
+    }
+}
